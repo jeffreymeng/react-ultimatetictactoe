fe617b49ff8f0a53806970563c0388388dc4482d
Jeffrey Meng <jeffkmeng@gmail.com>
2018-11-07 23:20:55 -0800

    add build

---
+++ docs/precache-manifest.7e6857926da5e2c17d77a544e443450e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "589056ec1315473bb614",
+    "url": "/react-ultimatetictactoe/static/js/runtime~main.589056ec.js"
+  },
+  {
+    "revision": "dab7e15b91ccebfc725d",
+    "url": "/react-ultimatetictactoe/static/js/main.dab7e15b.chunk.js"
+  },
+  {
+    "revision": "8649b2a3c146a3d374b2",
+    "url": "/react-ultimatetictactoe/static/js/1.8649b2a3.chunk.js"
+  },
+  {
+    "revision": "dab7e15b91ccebfc725d",
+    "url": "/react-ultimatetictactoe/static/css/main.93477647.chunk.css"
+  },
+  {
+    "revision": "e314459afdc89871a7cceb8d2d05fea0",
+    "url": "/react-ultimatetictactoe/index.html"
+  }
+];
